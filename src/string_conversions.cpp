cc948472093718134e551693e5acae88f9ecbe7b
jvsr <jellevisser00@gmail.com>
2021-03-31 15:00:46 +0200

    Change string conversion to general namespace

--- src/string_conversions.cpp
+++ src/string_conversions.cpp
@@ -1,20 +1,18 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        ::::::::            */
-/*   str_to_short.cpp                                   :+:    :+:            */
+/*   string_conversions.cpp                             :+:    :+:            */
 /*                                                     +:+                    */
 /*   By: jsaariko <jsaariko@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2021/03/31 13:39:29 by jsaariko      #+#    #+#                 */
-/*   Updated: 2021/03/31 13:50:14 by jsaariko      ########   odam.nl         */
+/*   Updated: 2021/03/31 14:42:37 by jvisser       ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <sstream>
-#include <string>
-#include <climits>
+#include "string_conversions.h"
 
-uint16_t strToShort(std::string value) {
+uint16_t StringConversion::toUint16(const std::string& value) {
     int num;
     std::stringstream ss;
 
@@ -22,7 +20,7 @@ uint16_t strToShort(std::string value) {
     ss >> num;
     if ((num == 0 && value != "0")
     || (num > USHRT_MAX || num < 0)) {
-        throw std::exception();
+        throw std::out_of_range("Cannot convert string to uint16");
     }
     return (static_cast<uint16_t>(num));
 }
---
+++ src/string_conversions.h
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        ::::::::            */
+/*   string_conversions.h                               :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: jvisser <jvisser@student.codam.nl>           +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2021/03/31 14:00:45 by jvisser       #+#    #+#                 */
+/*   Updated: 2021/03/31 14:37:04 by jvisser       ########   odam.nl         */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef STRING_CONVERSIONS_H_
+#define STRING_CONVERSIONS_H_
+
+#include <string>
+#include <sstream>
+#include <climits>
+#include <cinttypes>
+
+namespace StringConversion {
+    uint16_t toUint16(const std::string& value);
+}
+
+#endif  // STRING_CONVERSIONS_H_
