c397b0411f24322b89e6e5597e5d14e160c61b28
saarikoski-jules <jules.saarikoski@gmail.com>
2021-04-13 18:16:04 +0200

    fixed replies

--- src/reply.cpp
+++ src/reply.cpp
@@ -6,7 +6,7 @@
 /*   By: jvisser <jvisser@student.codam.nl>           +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2021/04/09 15:41:18 by jvisser       #+#    #+#                 */
-/*   Updated: 2021/04/13 17:30:58 by jsaariko      ########   odam.nl         */
+/*   Updated: 2021/04/13 18:15:22 by jsaariko      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,11 +15,12 @@
 #include <string>
 #include <vector>
 #include <sstream>
-
+#include <iostream>
 std::string ReplyFactory::newReply(const ReplyCode& code, std::vector<std::string> params) {
     std::stringstream ss;
 
     ss << code << " " << params[0] << " ";
+    std::cout << code << std::endl;
     switch (code)
     {
     case ERR_NONICKNAMEGIVEN:
@@ -30,12 +31,16 @@ std::string ReplyFactory::newReply(const ReplyCode& code, std::vector<std::strin
         break;
     case ERR_BADCHANNELKEY:
         ss << ":Cannot join channel (+k)";
+        break;
     case ERR_NOSUCHCHANNEL:
         ss << ":No such channel";
+        break;
     case ERR_NEEDMOREPARAMS:
         ss << ":Not enough parameters";
+        break;
     default:
         break;
     }
+    std::cout << ss.str() << std::endl;
     return (ss.str());
 }
