5f3d576220c6b5eeeb344d986d49e36e98c42677
saarikoski-jules <jsaariko@student.codam.nl>
2021-05-10 14:17:15 +0200

    better leaf connection matching function

--- src/connection.cpp
+++ src/connection.cpp
@@ -6,7 +6,7 @@
 /*   By: jvisser <jvisser@student.codam.nl>           +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2021/04/16 16:07:44 by jvisser       #+#    #+#                 */
-/*   Updated: 2021/05/04 13:22:57 by jsaariko      ########   odam.nl         */
+/*   Updated: 2021/05/10 14:16:47 by jsaariko      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,13 +30,27 @@ connectionType(Connection::NoType),
 server(serverConfiguration) {
 }
 
+bool matchPrefix(const std::string& prefix, const std::string& nick) {
+	std::string::const_iterator endOfNick = prefix.begin();
+	for (; endOfNick != prefix.end(); endOfNick++) {
+		if ((*endOfNick > '9' || *endOfNick < '0')
+        && (*endOfNick > 'z' || *endOfNick < 'a')
+        && (*endOfNick > 'Z' || *endOfNick < 'A')) {
+			break;
+		}
+	}
+	std::string nickFromPrefix(prefix.begin(), endOfNick);
+	if (nickFromPrefix == nick) {
+		return (true);
+	}
+	return (false);
+}
+
 Connection* Connection::getLeafConnection(const std::string& str) {
 	for (std::vector<Connection>::iterator i = leafConnections.begin(); i != leafConnections.end(); i++) {
-		//TODO(Jules): Make a better matching function
-		if (i->connectionType == ClientType && i->client.nickName == str) {
+		if (i->connectionType == ClientType && matchPrefix(str, i->client.nickName)) {
 			return (&*i);
 		}
-		//TOOD(Jules): also check other server connections
 	}
 	throw std::out_of_range("Coundn't find matching connection in leaves");
 }
