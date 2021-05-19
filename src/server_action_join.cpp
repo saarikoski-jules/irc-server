9b8da7158c3a4af421c6f118f72fdce38e6f6d44
saarikoski-jules <jsaariko@student.codam.nl>
2021-05-19 08:05:07 +0200

    yeet unused dummy function

--- src/server_action_join.cpp
+++ src/server_action_join.cpp
@@ -6,7 +6,7 @@
 /*   By: jsaariko <jsaariko@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2021/04/20 11:17:13 by jsaariko      #+#    #+#                 */
-/*   Updated: 2021/05/18 12:31:06 by jsaariko      ########   odam.nl         */
+/*   Updated: 2021/05/19 08:03:48 by jsaariko      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,13 +24,6 @@ ServerActionJoin::ServerActionJoin(
     std::vector<std::string> params, const int& fd, const std::string& prefix) :
 IServerAction(fd, 1, prefix),
 params(params) {}
-
-Channel* ServerActionJoin::getChannel(
-    const std::string&, const std::string&) {
-    Channel* chan = NULL;
-
-    return (chan);
-}
 
 void ServerActionJoin::broadcastJoin(Channel* chan) {
     std::vector<Connection*> sendTo = chan->getConnections(*connection);
