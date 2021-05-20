3a34077d22770e4dd9b6ea0984a59eacf05f1249
saarikoski-jules <jules.saarikoski@gmail.com>
2021-05-20 15:37:24 +0200

    dat feeling when you accidentally yeet main.cpp

---
+++ src/main.cpp
@@ -0,0 +1,30 @@
+
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        ::::::::            */
+/*   main.cpp                                           :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: jvisser <jvisser@student.codam.nl>           +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2021/03/31 09:04:39 by jvisser       #+#    #+#                 */
+/*   Updated: 2021/05/05 12:28:08 by jsaariko      ########   odam.nl         */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "start_irc_server.h"
+
+#include "server.h"
+#include "logger.h"
+
+int main(int argc, char** argv) {
+    try {
+		Logger::log(LogLevelInfo, "Program started");
+        startIrcServerFromArguments(argc, argv);
+    } catch (const ArgumentException& e) {
+        Logger::log(LogLevelFatal, e.what());
+    } catch (const ServerException& e) {
+        Logger::log(LogLevelFatal, e.what());
+    }
+    Logger::log(LogLevelInfo, "Program ended gracefully");
+    return (0);
+}
